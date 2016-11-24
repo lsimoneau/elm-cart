ea7f6f45784ce1ae8e647e3c98b1aee977397a26
Louis Simoneau <simoneau.louis@gmail.com>
2016-11-24 11:04:21 +1100

    Format and refactor

--- app/assets/elm/ShoppingCart.elm
+++ app/assets/elm/ShoppingCart.elm
@@ -27,13 +27,13 @@ type alias Model =
     }
 
 
-init : (Model, Cmd Msg)
+init : ( Model, Cmd Msg )
 init =
-    (
-      { loading = False
+    ( { loading = False
       , cart = fromList []
       }
-    , fetchCart)
+    , fetchCart
+    )
 
 
 
@@ -43,23 +43,23 @@ init =
 type Msg
     = Increment ProductId
     | Decrement ProductId
-    | LoadCart (Result Http.Error Cart)
+    | UpdateCart (Result Http.Error Cart)
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment productId ->
-          (model, updateItem productId 1)
+            ( model, updateItem productId 1 )
 
         Decrement productId ->
-          (model, updateItem productId -1)
+            ( model, updateItem productId -1 )
 
-        LoadCart (Ok cart) ->
-            ({ model | cart = cart }, Cmd.none)
+        UpdateCart (Ok cart) ->
+            ( { model | cart = cart }, Cmd.none )
 
-        LoadCart (Err _) ->
-            (model, Cmd.none)
+        UpdateCart (Err _) ->
+            ( model, Cmd.none )
 
 
 
@@ -77,69 +77,74 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  section [class "section"]
-  [ div [] (List.map itemView (toList model.cart)) ]
+    section [ class "section" ]
+        [ div [] (List.map itemView (toList model.cart)) ]
 
 
 itemView : ( ProductId, Item ) -> Html Msg
 itemView ( productId, item ) =
-  div [class "box"]
-  [
-    article [class "product media"]
-      [ div [class "content"]
-        [ div [] [ text item.name ]
-        , div [] [ toString item.quantity |> text ]
-        , div [] [ dollars item.subtotal |> text ]
-        , button [ onClick (Decrement productId) ] [ text "-" ]
-        , button [ onClick (Increment productId) ] [ text "+" ]
+    div [ class "box" ]
+        [ article [ class "product media" ]
+            [ div [ class "content" ]
+                [ div [] [ text item.name ]
+                , div [] [ toString item.quantity |> text ]
+                , div [] [ dollars item.subtotal |> text ]
+                , button [ onClick (Decrement productId) ] [ text "-" ]
+                , button [ onClick (Increment productId) ] [ text "+" ]
+                ]
+            ]
         ]
-      ]
-    ]
 
 
 dollars : Float -> String
 dollars n =
-  "$" ++ Round.round 2 n
+    "$" ++ Round.round 2 n
+
+
 
 -- HTTP
 
 
 updateItem : ProductId -> Int -> Cmd Msg
 updateItem id quantity =
-  Http.send LoadCart (Http.post "/cart/items" (Http.jsonBody (Encode.object [("product_id", Encode.int id), ("quantity", Encode.int quantity)])) decodeCart)
+    Http.send UpdateCart (Http.post "/cart/items" (Http.jsonBody (Encode.object [ ( "product_id", Encode.int id ), ( "quantity", Encode.int quantity ) ])) decodeCart)
 
 
 fetchCart : Cmd Msg
 fetchCart =
-  Http.send LoadCart (Http.get "/cart.json" decodeCart)
+    Http.send UpdateCart (Http.get "/cart.json" decodeCart)
 
 
 type alias JsonItem =
-  { productId: Int, quantity: Int, productName: String, unitPrice: Float, subtotal: Float }
+    { productId : Int, quantity : Int, productName : String, unitPrice : Float, subtotal : Float }
 
 
 decodeCart : Decode.Decoder Cart
 decodeCart =
-  Decode.map buildCart (Decode.field "items" (Decode.list decodeItem))
+    Decode.map buildCart (Decode.field "items" (Decode.list decodeItem))
 
 
 buildCart : List JsonItem -> Cart
 buildCart items =
-  Dict.fromList (List.map (\i ->
-    (i.productId, { quantity = i.quantity, name = i.productName, unitPrice = i.unitPrice, subtotal = i.subtotal })
-  ) items)
+    Dict.fromList
+        (List.map
+            (\i ->
+                ( i.productId, { quantity = i.quantity, name = i.productName, unitPrice = i.unitPrice, subtotal = i.subtotal } )
+            )
+            items
+        )
 
 
 decodeItem : Decode.Decoder JsonItem
 decodeItem =
-  Decode.map5 JsonItem
-    (Decode.field "productId" Decode.int)
-    (Decode.field "quantity" Decode.int)
-    (Decode.field "productName" Decode.string)
-    (Decode.field "unitPrice" (Decode.map parseDecimal Decode.string))
-    (Decode.field "subtotal" (Decode.map parseDecimal Decode.string))
+    Decode.map5 JsonItem
+        (Decode.field "productId" Decode.int)
+        (Decode.field "quantity" Decode.int)
+        (Decode.field "productName" Decode.string)
+        (Decode.field "unitPrice" (Decode.map parseDecimal Decode.string))
+        (Decode.field "subtotal" (Decode.map parseDecimal Decode.string))
 
 
 parseDecimal : String -> Float
 parseDecimal str =
-  Result.withDefault 0 (String.toFloat str)
+    Result.withDefault 0 (String.toFloat str)
