68f13b4541d6d566eb90a483a54e0c856ac66674
Louis Simoneau <simoneau.louis@gmail.com>
2016-12-02 07:06:14 +1100

    Extract View into a module

--- app/assets/elm/ShoppingCart.elm
+++ app/assets/elm/ShoppingCart.elm
@@ -2,15 +2,13 @@ port module ShoppingCart exposing (..)
 
 import ShoppingCart.Types exposing (..)
 import ShoppingCart.Api exposing (..)
+import ShoppingCart.View exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html exposing (program)
 import Http exposing (Error, Response)
 import Dict exposing (fromList, toList)
 import Json.Decode as Decode exposing (..)
 import Json.Encode as Encode exposing (..)
-import Round exposing (round)
 
 
 main =
@@ -44,7 +42,6 @@ emptyCard =
 
 port checkout : CardDetails -> Cmd msg
 
-
 port cardToken : (String -> msg) -> Sub msg
 
 
@@ -69,12 +66,8 @@ update msg model =
         UpdateCart (Err _) ->
             ( model, Cmd.none )
 
-        PaymentSubmitted (Ok True) ->
+        PaymentSubmitted (Ok _) ->
             ( { model | formSubmitting = False, paymentError = Nothing }, Cmd.none )
-
-        -- Got response from server, but charge has { paid: false }
-        PaymentSubmitted (Ok False) ->
-            ( { model | formSubmitting = False, paymentError = Just "Oops, something went wrong" }, Cmd.none )
 
         PaymentSubmitted (Err error) ->
             ( { model | formSubmitting = False, paymentError = Just (handleError error) }, Cmd.none )
@@ -135,82 +128,3 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     cardToken GetCardToken
 
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    section [ class "section" ]
-        [ div [] (List.map itemView model.cart)
-        , a [ class "button is-primary", onClick GoToCheckout ] [ text "Checkout" ]
-        , checkoutDialog model
-        ]
-
-
-itemView : Item -> Html Msg
-itemView item =
-    div [ class "box" ]
-        [ article [ class "product media" ]
-            [ div [ class "content" ]
-                [ div [] [ text item.name ]
-                , div [] [ toString item.quantity |> text ]
-                , div [] [ dollars item.subtotal |> text ]
-                , button [ onClick (Decrement item.productId) ] [ text "-" ]
-                , button [ onClick (Increment item.productId) ] [ text "+" ]
-                ]
-            ]
-        ]
-
-
-checkoutDialog : Model -> Html Msg
-checkoutDialog model =
-    div [ class (if model.checkout then "modal is-active" else "modal") ]
-        [ div [ class "modal-background" ] []
-        , div [ class "modal-card" ]
-            [ header [ class "modal-card-head" ]
-                [ p [ class "modal-card-title" ] [ text "Checkout" ] ]
-            , section [ class "modal-card-body" ]
-                [ formError model.paymentError
-                , label [ class "label" ] [ text "Card Number" ]
-                , p [ class "control has-icon has-icon-left" ]
-                    [ i [ class "fa fa-credit-card" ] []
-                    , input [ class "input", type_ "text", onInput UpdateCardNumber ] []
-                    ]
-                , div [ class "columns" ]
-                    [ div [ class "column" ]
-                        [ label [ class "label" ] [ text "Expiry" ]
-                        , p [ class "control has-addons has-icon has-icon-left" ]
-                            [ i [ class "fa fa-calendar" ] []
-                            , input [ class "input", size 2, type_ "text", placeholder "MM", onInput UpdateExpMonth ] []
-                            , input [ class "input", size 2, type_ "text", placeholder "YY", onInput UpdateExpYear ] []
-                            ]
-                        ]
-                    , div [ class "column" ]
-                        [ label [ class "label" ] [ text "CVC" ]
-                        , p [ class "control has-icon has-icon-left" ]
-                            [ i [ class "fa fa-lock" ] []
-                            , input [ class "input", type_ "text", onInput UpdateCvc ] []
-                            ]
-                        ]
-                    ]
-                ]
-            , footer [ class "modal-card-foot" ]
-                [ a [ class (if model.formSubmitting then "button is-primary is-loading" else "button is-primary"), onClick Checkout ] [ text "Checkout" ]
-                , a [ class "button", onClick CancelCheckout ] [ text "Cancel" ]
-                ]
-            ]
-        ]
-
-
-formError : Maybe String -> Html Msg
-formError error =
-  case error of
-    Just errorText -> div [ class "notification is-danger" ] [ text errorText ]
-    Nothing -> div [] []
-
-
-dollars : Float -> String
-dollars n =
-    "$" ++ Round.round 2 n
---
+++ app/assets/elm/ShoppingCart/View.elm
@@ -0,0 +1,88 @@
+module ShoppingCart.View exposing (..)
+
+import ShoppingCart.Types exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Round exposing (round)
+
+
+view : Model -> Html Msg
+view model =
+    section [ class "section" ]
+        [ div [] (List.map itemView model.cart)
+        , a [ class "button is-primary", onClick GoToCheckout ] [ text "Checkout" ]
+        , checkoutDialog model
+        ]
+
+
+itemView : Item -> Html Msg
+itemView item =
+    div [ class "box" ]
+        [ article [ class "product media" ]
+            [ div [ class "content" ]
+                [ div [] [ text item.name ]
+                , div [] [ toString item.quantity |> text ]
+                , div [] [ dollars item.subtotal |> text ]
+                , button [ onClick (Decrement item.productId) ] [ text "-" ]
+                , button [ onClick (Increment item.productId) ] [ text "+" ]
+                ]
+            ]
+        ]
+
+
+checkoutDialog : Model -> Html Msg
+checkoutDialog model =
+    div [ class (if model.checkout then "modal is-active" else "modal") ]
+        [ div [ class "modal-background" ] []
+        , div [ class "modal-card" ]
+            [ header [ class "modal-card-head" ]
+                [ p [ class "modal-card-title" ] [ text "Checkout" ] ]
+            , section [ class "modal-card-body" ]
+                [ checkoutForm model ]
+            , footer [ class "modal-card-foot" ]
+                [ a [ class (if model.formSubmitting then "button is-primary is-loading" else "button is-primary"), onClick Checkout ] [ text "Checkout" ]
+                , a [ class "button", onClick CancelCheckout ] [ text "Cancel" ]
+                ]
+            ]
+        ]
+
+checkoutForm : Model -> Html Msg
+checkoutForm model =
+    div []
+        [ formError model.paymentError
+        , label [ class "label" ] [ text "Card Number" ]
+        , p [ class "control has-icon has-icon-left" ]
+            [ i [ class "fa fa-credit-card" ] []
+            , input [ class "input", type_ "text", onInput UpdateCardNumber ] []
+            ]
+        , div [ class "columns" ]
+            [ div [ class "column" ]
+                [ label [ class "label" ] [ text "Expiry" ]
+                , p [ class "control has-addons has-icon has-icon-left" ]
+                    [ i [ class "fa fa-calendar" ] []
+                    , input [ class "input", size 2, maxlength 2, type_ "text", placeholder "MM", onInput UpdateExpMonth ] []
+                    , input [ class "input", size 2, maxlength 2, type_ "text", placeholder "YY", onInput UpdateExpYear ] []
+                    ]
+                ]
+            , div [ class "column" ]
+                [ label [ class "label" ] [ text "CVC" ]
+                , p [ class "control has-icon has-icon-left" ]
+                    [ i [ class "fa fa-lock" ] []
+                    , input [ class "input", type_ "text", onInput UpdateCvc ] []
+                    ]
+                ]
+            ]
+        ]
+
+formError : Maybe String -> Html Msg
+formError error =
+  case error of
+    Just errorText -> div [ class "notification is-danger" ] [ text errorText ]
+    Nothing -> div [] []
+
+
+dollars : Float -> String
+dollars n =
+    "$" ++ Round.round 2 n
