1dacee7a6cb0c2940e9d5a10be3aa483048e539a
Louis Simoneau <simoneau.louis@gmail.com>
2017-01-12 07:03:12 +1100

    Fix elm tests and improve formatting

--- spec/elm/Api.elm
+++ spec/elm/Api.elm
@@ -9,35 +9,37 @@ import Json.Decode exposing (..)
 
 
 cartJson : String
-cartJson = """
-{
-  "id":55,
-  "items": [
+cartJson =
+    """
     {
-      "productId": 1,
-      "productName":"Sulfuras, hand of Ragnaros",
-      "quantity":1,
-      "unitPrice":"999.0",
-      "subtotal":"999.0"
+      "id":55,
+      "items": [
+        {
+          "productId": 1,
+          "productName":"Sulfuras, hand of Ragnaros",
+          "quantity":1,
+          "unitPrice":"999.0",
+          "subtotal":"999.0"
+        }
+      ],
+      "total": "999.0"
     }
-  ]
-}
-"""
+    """
 
 
 expectedCart : Cart
 expectedCart =
-  Dict.fromList [(1, ({ name = "Sulfuras, hand of Ragnaros", quantity = 1, unitPrice = 999.0, subtotal = 999.0 }))]
+    { items = [ { productId = 1, name = "Sulfuras, hand of Ragnaros", quantity = 1, unitPrice = 999.0, subtotal = 999.0 } ], total = 999.0 }
 
 
 all : Test
 all =
-  describe "JSON decoding"
-  [ describe "Cart JSON decoder"
-      [ test "decodes JSON" <|
-          \() ->
-            cartJson
-              |> Json.Decode.decodeString cartDecoder
-              |> Expect.equal (Ok expectedCart)
-      ]
-  ]
+    describe "JSON decoding"
+        [ describe "Cart JSON decoder"
+            [ test "decodes JSON" <|
+                \() ->
+                    cartJson
+                        |> Json.Decode.decodeString cartDecoder
+                        |> Expect.equal (Ok expectedCart)
+            ]
+        ]
